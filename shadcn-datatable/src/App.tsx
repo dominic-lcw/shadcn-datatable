6b057e05c655aadbb6fd55ac8d624abf88b475eb
Dominic Leung <leungcheuk209@gmail.com>
2025-07-10 00:13:45 +0800

    add configs

--- shadcn-datatable/src/App.tsx
+++ shadcn-datatable/src/App.tsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Button } from "@/components/ui/button";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="grid grid-cols-4 gap-4 items-center justify-center p-10 mx-auto">
+      <div className="border p-4 flex items-center justify-center h-128">
+        Component 1
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="border p-4 flex items-center justify-center h-128">
+        Component 2
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <div className="border p-4 flex items-center justify-center h-128">
+        Component 3
+      </div>
+      <div className="border p-4 flex items-center justify-center h-128">
+        Component 4
+      </div>
+      <div className="border p-4 flex items-center justify-center h-128">
+        Component 5
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
